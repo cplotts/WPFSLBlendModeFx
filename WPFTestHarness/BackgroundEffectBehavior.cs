2c76248207407a875274722c576db18fc4ed0c5f
Cory Plotts <cory_plotts@starkey.com>
2025-01-25 06:11:09 -0600

    Convert to .NET 4.8
    Got things compiling but the blend modes aren't working. Sadness.

--- WPFTestHarness/BackgroundEffectBehavior.cs
+++ WPFTestHarness/BackgroundEffectBehavior.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Microsoft.Xaml.Behaviors;
+using System;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Effects;
 using System.Windows.Shapes;
-using System.Windows.Interactivity;
 
 namespace WPFTestHarness
 {
